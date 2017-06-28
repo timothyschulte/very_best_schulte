1312538a386f586f53eae856b850d674a29d8fca
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:13:38 +0000

    Direct association between venues and menu_items

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,8 @@
 class MenuItem < ApplicationRecord
   # Direct associations
 
+  belongs_to :venue
+
   # Indirect associations
 
   # Validations
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,9 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  has_many   :menu_items,
+             :dependent => :nullify
+
   # Indirect associations
 
   # Validations
