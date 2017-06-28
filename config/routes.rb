e40db86bebe815b34d3c758cd940019eb3ec69c8
Raghu Betina <raghu@firstdraft.com>
2017-06-28 02:13:13 +0000

    Generate venues resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Venue resource:
+  # CREATE
+  get "/venues/new", :controller => "venues", :action => "new"
+  post "/create_venue", :controller => "venues", :action => "create"
+
+  # READ
+  get "/venues", :controller => "venues", :action => "index"
+  get "/venues/:id", :controller => "venues", :action => "show"
+
+  # UPDATE
+  get "/venues/:id/edit", :controller => "venues", :action => "edit"
+  post "/update_venue/:id", :controller => "venues", :action => "update"
+
+  # DELETE
+  get "/delete_venue/:id", :controller => "venues", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20170628021313_create_venues.rb
@@ -0,0 +1,13 @@
+class CreateVenues < ActiveRecord::Migration
+  def change
+    create_table :venues do |t|
+      t.string :name
+      t.string :address
+      t.integer :neighborhood_id
+      t.integer :cuisine_id
+
+      t.timestamps
+
+    end
+  end
+end
